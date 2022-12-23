f1f9dd623cb9561e01d9019dff253fe23823f829
schnmnn <manuelbruecker@yahoo.de>
2022-12-23 16:41:09 +0100

    update

--- Day3/day3.py
+++ Day3/day3.py
@@ -32,4 +32,40 @@ for count_item_rucksack in range(len_rucksack):
 wrong_items = pd.DataFrame(list_of_wrong_items)
 wrong_items = wrong_items.set_axis(['item'],axis='columns')
 wrong_items = pd.merge(wrong_items,prio_matching,on='item')
-print(wrong_items['priorities'].sum())
+print('The answer to part one is ' + str(wrong_items['priorities'].sum()))
+
+
+### Part two ###
+prio_matching = pd.read_csv('prio_matching.csv')
+len_input = len(input)
+count1=0
+count2=2
+all_rucksacks = []
+for i in range(0,len_input,3):
+    group_of_3 = []
+    a = input['rucksack'].loc[count1:count2].astype(str)
+    b = a.values.tolist()
+    group_of_3.append(b)
+    all_rucksacks.append(b)
+    count1=count1+3
+    count2=count2+3
+    
+len_all_rucksacks = len(all_rucksacks)
+
+badge=[]
+for rucksack in range(len_all_rucksacks):
+    one_and_two = []
+    one_and_two_and_three=[]
+    for i in all_rucksacks[rucksack][0]:
+        if i in all_rucksacks[rucksack][1] and i not in one_and_two:
+            one_and_two.append(i)
+    for i in one_and_two:
+        if i in all_rucksacks[rucksack][2] and i not in one_and_two_and_three:
+            one_and_two_and_three.append(i)
+    badge.append(one_and_two_and_three)
+
+df = pd.DataFrame(badge,columns=['item'])
+df_points = pd.merge(df,prio_matching,on='item')
+answer_2 = df_points['priorities'].sum()
+
+print('The answer to part two is '+str(answer_2))
